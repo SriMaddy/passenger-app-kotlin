9b931e19ba3fdd54dbb4e59b2540c27bce36558f
Sridhar <bosoftech@gmail.com>
2018-12-19 17:30:11 +0530

    project initialized

---
+++ app/src/main/java/learning/kotlin/com/itsvisiblepassengerusingkotlin/MainActivity.kt
@@ -0,0 +1,12 @@
+package learning.kotlin.com.itsvisiblepassengerusingkotlin
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
